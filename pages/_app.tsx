9d33066ea4d883f3d41e946643b482e11f3b3c7d
Lukas Oppermann <oppermann.lukas@gmail.com>
2021-01-11 11:55:56 +0100

    add basic page animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,24 @@
 import Navigation from '@components/Navigation'
 import Footer from '@components/Footer'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import { useRouter } from 'next/router'
 import '@styles/globals.scss'
 
 function MyApp ({ Component, pageProps }) {
+  const { asPath } = useRouter()
+
   return (
     <>
       <Navigation />
-      <Component {...pageProps} />
+      <TransitionGroup>
+        <CSSTransition
+          key={asPath}
+          classNames='Page-animation__fade'
+          timeout={1000}
+        >
+          <Component {...pageProps} />
+        </CSSTransition>
+      </TransitionGroup>
       <Footer />
     </>
   )
