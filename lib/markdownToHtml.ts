3f6f7366d682ccf10d724e9bed08843a9c0e57eb
Lukas Oppermann <oppermann.lukas@gmail.com>
2021-08-27 09:06:00 +0200

    fix imprint mobile & replace outdated markdown deps

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -2,6 +2,8 @@ import remark from 'remark'
 // import html from 'remark-html'
 import { unified } from 'unified'
 import remarkParse from 'remark-parse'
+import remarkGfm from 'remark-gfm'
+import rehypeRaw from 'rehype-raw'
 import remarkRehype from 'remark-rehype'
 import rehypeFormat from 'rehype-format'
 import rehypeStringify from 'rehype-stringify'
@@ -9,7 +11,9 @@ import rehypeStringify from 'rehype-stringify'
 export default async function markdownToHtml (markdown) {
   const result = await unified()
     .use(remarkParse)
-    .use(remarkRehype)
+    .use(remarkGfm)
+    .use(remarkRehype, {allowDangerousHtml: true})
+    .use(rehypeRaw)
     .use(rehypeFormat)
     .use(rehypeStringify)
     .process(markdown)
