821901e4560c0504ee192f3e4ae3eebf7e88ec60
Lukas Oppermann <oppermann.lukas@gmail.com>
2022-06-20 13:04:50 +0200

    add redirect to resume pdf

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = withBundleAnalyzer({
     return [
       {
         source: '/resume',
-        destination: 'https://images.ctfassets.net/5dfliyp93yzg/cjGKGKXUMxAaOVJg53FHI/f3d3c9a2a176335affec167154b6881c/resume_lukas_oppermann_01.4.pdf'
+        destination: '/resume_lukas_oppermann.pdf'
       }
     ]
   },
