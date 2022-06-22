e41dfaba3a9b07dacf54b5cb4c13b8007ce4051a
Lukas Oppermann <oppermann.lukas@gmail.com>
2022-06-22 08:53:13 +0200

    remove cypress support folder

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
     baseUrl: 'http://localhost:3000',
-    video: false
+    video: false,
+    supportFile: false
   },
 })
