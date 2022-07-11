7c90aee7f69ee02f46dbcd63df4613f7e36bab57
Lukas Oppermann <oppermann.lukas@gmail.com>
2022-07-11 10:20:08 +0200

    rename test

--- cypress/e2e/imprint.cy.js
+++ cypress/e2e/imprint.cy.js
@@ -3,7 +3,7 @@
 // If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
 // https://github.com/cypress-io/eslint-plugin-cypress
 
-describe('Navigation', () => {
+describe('Imprint', () => {
   it('should navigate to the imprint page', () => {
     // Start from the index page
     cy.visit('/')
