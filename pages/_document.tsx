256d240acb3940a9991b70db1f5f915e9f36325a
Lukas Oppermann <lukasoppermann@github.com>
2022-09-30 15:04:32 +0200

    fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     return (
       <Html lang='en'>
         <Head>
-          <link href='https://fonts.googleapis.com/css?family=Montserrat:700|Noto+Serif:400,400i,400b|Source+Sans+Pro:400,600|Source+Code+Pro:700&display=swap' rel='stylesheet' />
+          {/* <link href='https://fonts.googleapis.com/css?family=Montserrat:700|Noto+Serif:400,400i,400b|Source+Sans+Pro:400,600|Source+Code+Pro:700&display=swap' rel='stylesheet' /> */}
           <meta name="color-scheme" content="light dark" />
         </Head>
         <body>
