3843900e8045f9921b001187f212b153b9115b54
Lukas Oppermann <lukasoppermann@github.com>
2023-06-19 11:30:14 +0200

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
-// https://astro.build/config
 import image from "@astrojs/image";
 
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [sitemap(), image({
     serviceEntryPoint: "@astrojs/image/sharp"
-  })]
+  })],
+  adapter: vercel()
 });
