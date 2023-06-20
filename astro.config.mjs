cdb57af4c5d0a0d7294adf1be2f20f04beb278dd
Lukas Oppermann <lukasoppermann@github.com>
2023-06-20 12:34:56 +0200

    reset to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
   integrations: [sitemap(), image({
     serviceEntryPoint: "@astrojs/image/sharp"
   })],
