c5275a283eaf092210cbc47643d5ac78395b75fb
Lukas Oppermann <oppermann.lukas@gmail.com>
2025-06-29 13:54:06 +0200

    fix

--- src/pages/resume.ts
+++
@@ -1,6 +0,0 @@
-// import type { APIContext } from 'astro';
-// import { resumePdfLink } from '@src/config'
-
-// export function get({ redirect }: APIContext) {
-//   return redirect(resumePdfLink, 302);
-// }
